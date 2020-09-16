76eed09c71fdcf225e65df40aab9fc25e80f4b69
Joseph <josdavil@espol.edu.ec>
2020-09-16 15:52:05 -0500

    old tweets

---
+++ main.py
@@ -0,0 +1,6 @@
+import GetOldTweets3 as got
+
+tweetCriteria = got.manager.TweetCriteria().setUsername("barackobama whitehouse")\
+                                           .setMaxTweets(2)
+tweet = got.manager.TweetManager.getTweets(tweetCriteria)[0]
+print(tweet.text)
